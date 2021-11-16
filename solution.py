f89d0c503fddf71bf747a079a3b993a1e40f86f6
Lailatef <54871132+Lailatef@users.noreply.github.com>
2021-11-16 13:53:30 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -101,7 +101,7 @@ def get_route(hostname):
                 
                 try:
 
-                    dest = gethostbyname(addr[0])
+                    dest = gethostbyaddr(addr[0])
                     
 
                 except herror:
